69585ba9b7ac1ba46322b8ed3e4a9841142dbbbf
huaiyinfeilong <huaiyinfeilong@163.com>
2023-05-12 17:56:42 +0800

    初始化项目。

---
+++ AudioRouteTest/AudioRouteTest.cpp
@@ -0,0 +1,13 @@
+﻿// AudioRouteTest.cpp
+// 音频路由测试
+//
+
+#include <iostream>
+#include <atlbase.h>
+#include <log4cxx/logger.h>
+
+log4cxx::LoggerPtr logger = log4cxx::Logger::getRootLogger();
+
+int main()
+{
+}
