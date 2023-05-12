a9ff41dd312883cc8cfa3c43be56ddf7454b2b73
huaiyinfeilong <huaiyinfeilong@163.com>
2023-05-12 18:37:20 +0800

    update

--- AudioRouteTest/AudioRouteTest.cpp
+++ AudioRouteTest/AudioRouteTest.cpp
@@ -5,9 +5,24 @@
 #include <iostream>
 #include <atlbase.h>
 #include <log4cxx/logger.h>
+#include <Windows.h>
 
 log4cxx::LoggerPtr logger = log4cxx::Logger::getRootLogger();
 
+
 int main()
 {
+	// COM线程初始化
+	HRESULT hr;
+	hr = CoInitialize(nullptr);
+	if (FAILED(hr))
+	{
+		LOG4CXX_ERROR(logger, _T("CoInitialize()初始化失败，错误码：") << GetLastError() << _T("，程序退出。"));
+		return 0;
+	}
+	// COM线程初始化成功，开始执行业务
+	LOG4CXX_INFO(logger, _T("初始化成功。"));
+	// 业务执行完成，COM线程清理
+	CoUninitialize();
+	LOG4CXX_INFO(logger, _T("逆初始化完成，程序退出。"));
 }
